efafa7f4d064100e37857aac73ae2676e0d52cc2
Yomal Herath <yomalherath1121@gmail.com>
2020-10-22 00:07:54 +0530

    Create side nav bar component

---
+++ Frontend/src/app/side-nva-bar/side-nva-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nva-bar',
+  templateUrl: './side-nva-bar.component.html',
+  styleUrls: ['./side-nva-bar.component.scss']
+})
+export class SideNvaBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
